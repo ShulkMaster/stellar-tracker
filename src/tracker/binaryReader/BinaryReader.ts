fcd2f6d5f553bc959061cbd484853c649fc90954
shulkmaster <shulkmaster@gmail.com>
2026-03-07 22:50:45 -0600

    adding basic attribs to the BinaryReader.ts

--- src/tracker/binaryReader/BinaryReader.ts
+++ src/tracker/binaryReader/BinaryReader.ts
@@ -1,15 +1,19 @@
 export class BinaryReader {
+    private readonly _size = 0;
     private readonly _stream: ReadableStream<Uint8Array<ArrayBuffer>>;
+    private _position = 0;
 
-    constructor(stream: ReadableStream<Uint8Array<ArrayBuffer>>) {
-        this._stream = stream;
+    constructor(fileSize: number, stream: ReadableStream<Uint8Array<ArrayBuffer>>) {
+      this._size = fileSize;
+      this._stream = stream;
     }
 
     public get size(): number {
+      return this._size;
     }
 
     public get position(): number {
-
+      return this._position;
     }
 
     public readASCII(bytes: number): string {}
