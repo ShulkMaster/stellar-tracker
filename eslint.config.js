0ff2f3b820b381d263316c0b270f1637acf89a4d
shulkmaster <shulkmaster@gmail.com>
2026-03-07 21:21:59 -0600

    adding dependencies and configs for linter

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import litPlugin from 'eslint-plugin-lit';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+  },
+  {
+    plugins: {
+      lit: litPlugin,
+    },
+    rules: {
+      ...litPlugin.configs.recommended.rules,
+      'lit/no-useless-template-literals': 'error',
+      'lit/no-invalid-html': 'error',
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+  },
+  prettierConfig,
+  {
+    ignores: ['dist/', 'node_modules/'],
+  },
+);
