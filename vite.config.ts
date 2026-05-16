bf3f9ff136a0f54effa87a55850250dd78b787ec
shulkmaster <shulkmaster@gmail.com>
2026-05-16 13:01:01 -0600

    refactoring project as a Svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  test: {
-    globals: true,
-  },
+  plugins: [svelte()],
 });
